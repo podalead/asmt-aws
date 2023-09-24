9c55bbd37ac6341e8741ae44c19cf2e6ab04b527
Mykhail Poda <19616839+podalead@users.noreply.github.com>
2023-09-24 22:26:38 +0200

    up

--- terraform/vpc/gateways.tf
+++ terraform/vpc/gateways.tf
@@ -30,9 +30,9 @@ resource "aws_nat_gateway" "this" {
 }
 
 resource "aws_route" "private_nat_gateway" {
-  route_table_id         = element(aws_route_table.private[0].id, count.index)
+  route_table_id         = element(aws_route_table.private.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.this[0].id, count.index)
+  nat_gateway_id         = element(aws_nat_gateway.this.id, count.index)
 
   timeouts {
     create = "5m"
