ed24f62c9a4fbeec6578b0705f86a87d744c8786
Mykhail Poda <19616839+podalead@users.noreply.github.com>
2023-09-26 14:30:22 +0200

    update

--- terraform/eks/provider.tf
+++ terraform/eks/provider.tf
@@ -17,7 +17,7 @@ provider "aws" {}
 
 provider "kubernetes" {
   host                   = aws_eks_cluster.asmt_eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.asmt_eks_cluster.certificate_authority.data)
+  cluster_ca_certificate = base64decode(aws_eks_cluster.asmt_eks_cluster.certificate_authority.0.data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
