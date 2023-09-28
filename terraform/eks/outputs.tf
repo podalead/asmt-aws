fd06a029a4cd67df96edbb313d1a6dd62ab70f42
Mykhail Poda <19616839+podalead@users.noreply.github.com>
2023-09-28 18:16:02 +0200

    minor support scripts

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,5 +1,13 @@
-output "eks_endpoint" {
+output "eks_cluster_name" {
+  value = aws_eks_cluster.asmt_eks_cluster.name
+}
+
+output "eks_host" {
   value = aws_eks_cluster.asmt_eks_cluster.endpoint
+}
+
+output "eks_ca_cert_base64" {
+  value = aws_eks_cluster.asmt_eks_cluster.certificate_authority.0.data
 }
 
 output "eks_oidc_arn" {
